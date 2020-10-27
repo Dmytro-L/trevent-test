be312b15f43164f84b641f456dfb2f2e75c636fe
Dmytro-L <Lishnovskiy.dima@gmail.com>
2020-10-27 16:19:41 +0200

    coma

--- APP/js/main.js
+++ APP/js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     
     $('.burger').click(function(event) {
         $('.burger,.nav').toggleClass('active');
-        $('body').toggleClass('lock'); 
+        $('body').toggleClass('lock');
+        $('.backgr').toggleClass('yes'); 
     });
     
 });
