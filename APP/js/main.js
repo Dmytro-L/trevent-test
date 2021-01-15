bcf78cba069fa2d16cabb6eef2261d3d5d89aff3
Dmytro-L <Lishnovskiy.dima@gmail.com>
2021-01-15 22:28:34 +0200

    Commit name

--- APP/js/main.js
+++ APP/js/main.js
@@ -1,9 +1,29 @@
+new Swiper('.swiper-top', {
+    
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+})
+new Swiper('.swiper-clients', {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    slidesPerView: 1,
+    loop:true,
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+        },
+
+    },
+    
+})
+
 $(document).ready(function() {
-    
-    $('.burger').click(function(event) {
-        $('.burger,.nav').toggleClass('active');
+    $('.header__burger').click(function(event) {
+        $('.burger,.header__list').toggleClass('active');
         $('body').toggleClass('lock');
-        $('.backgr').toggleClass('yes'); 
     });
-    
 });
--- APP/js/main1.js
+++ APP/js/main1.js
@@ -4,45 +4,9 @@ $(document).ready(function() {
         $('body').toggleClass('lock');
         $('.home__main').toggleClass('none'); 
     });
-    $('.slider').slick({
-        slidesToShow:1,
-        dots:true,
-        speed:2000
-        
-    });
-    $('.slider1').slick({
-        slidesToShow:3,
-        speed:500,
-        responsive:[
-            {
-                breakpoint:750,
-                settings:{
-                    slidesToShow:1
-                }
-            }
-        ]
-        
-    });
-    $('.search__input').focus(function() {
-        $('.search__png').addClass('inputt');
-    }).blur(function() {
-        $('.search__png').removeClass('inputt');
-    });ww
 });
 
 
 
 
 
-//$('.slider').slick({
-    slidesToShow:4,
-    responsive:[
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow:2,
-            }
-        }
-    ]
-    
-});//
